ec53ad6e9197766eb1f74330c7e7619001221302
Christopher Herzog <cjh30@pct.edu>
2021-02-20 23:51:14 -0500

    added conditionals

--- dump/dump-lib.py
+++ dump/dump-lib.py
@@ -2,56 +2,93 @@
 # Purpose: Dump Library to integrate into applications
 
 #import logger
-import os
+
 
 class dump:
+    
+    import os
 
     # configure dump, allow defaults to be overwritten
-    
-    # get configuration information
     def dumper_loc(self, location=None):
-        if location is None:
+
+        dumper_loc_location = str(location)
+
+        if dumper_loc_location is None:
             raise Exception("Location cannot be None.")
-        elif location != None:
+        elif dumper_loc_location != None:
             # Test Run, not a valid location. For Testing purposes.
-            if location == 'Test' or location == 'test':
-                return(self.location)
+            if dumper_loc_location == 'Test' or dumper_loc_location == 'test':
+                return(dumper_loc_location)
 
             #check if location is directory
-            if os.path.exists(location) is False:
+            if os.path.exists(dumper_loc_location) is False:
                 raise Exception("Not a valid directory location.")
             else:
-                return(self.location)
-    
+                return(dumper_loc_location)
+
     def backup_dev(self, name=None):
-        if name is None:
-            raise Exception("Backup evice Name cannot be None.")
+
+        backup_dev_name = str(name)
+
+        if backup_dev_name is None:
+            raise Exception("Backup device Name cannot be None.")
         else:
-            return(self.name)
+            return(backup_dev_name)
 
     def input_dev(self, name=None):
-        if name is None:
+
+        input_dev_name = str(name)
+
+        if input_dev_name is None:
             raise Exception("Input device Name cannot be None.")
         else:
-            return(self.name)
+            return(input_dev_name)
 
     def backup_mntpnt(self, location='/mnt/'):
-        #check if location is directory
 
-        return(self.location)
-    
+        backup_mntpnt_location = str(location)
+
+        if backup_mntpnt_location is None:
+            raise Exception("Backup Location cannot be None.")
+        elif backup_mntpnt_location != None:
+            # Test Run, not a valid location. For Testing purposes.
+            if backup_mntpnt_location == 'Test' or backup_mntpnt_location == 'test':
+                return(backup_mntpnt_location)
+
+            #check if location is directory
+            if os.path.exists(backup_mntpnt_location) is False:
+                raise Exception("Not a valid directory location.")
+            else:
+                return(backup_mntpnt_location)
+
     def input_mntpnt(self, location='/mnt/'):
-        return(self.location)
+
+        input_mntpnt_location = str(location)
+
+        if input_mntpnt_location is None:
+            raise Exception("Input Location cannot be None.")
+        elif input_mntpnt_location != None:
+            # Test Run, not a valid location. For Testing purposes.
+            if input_mntpnt_location == 'Test' or input_mntpnt_location == 'test':
+                return(input_mntpnt_location)
+
+            #check if location is directory
+            if os.path.exists(input_mntpnt_location) is False:
+                raise Exception("Not a valid directory location.")
+            else:
+                return(input_mntpnt_location)
 
     # locate backup device < not configurable >
 
     # verify USB devices < not configurable >
 
-    # make udev rules < not configurable > 
+    # make udev rules < not configurable >
 
     # mount backup device < not configurable >
 
     # perform backup < not configurable >
 
     # unmount backup < not configurable >
+
+
 
